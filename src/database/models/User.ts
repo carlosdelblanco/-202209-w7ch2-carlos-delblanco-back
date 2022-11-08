31b3a252d0f78e143adb1fd9026774efea474d0e
Carlos del Blanco <delblanco.carlos@gmail.com>
2022-11-08 23:11:42 +0100

    Add user model and environment

---
+++ src/database/models/User.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: string,
+    required: true,
+    minlength: 6,
+  },
+});
